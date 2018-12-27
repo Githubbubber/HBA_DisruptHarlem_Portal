2bc3e826d90cf8a517554e3311af4302d2c38519
githubbubber <mekesiabrown@gmail.com>
2018-12-27 12:10:26 -0500

    Update js scripts

--- js/allscripts.js
+++ js/allscripts.js
@@ -1,42 +1,11 @@
-// For the slideshow modal on the individual game page
-function openModal() {
-  document.getElementById('myModal').style.display = "block";
-}
+$( ".select-tree" ).selectmenu({
+    icons: {
+        button: "glyphicon glyphicon-tree-conifer"
+    }
+});
 
-function closeModal() {
-  document.getElementById('myModal').style.display = "none";
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
-// For the slideshow modal on the individual game page
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+theDate = new Date(), 			
+today = months[theDate.getMonth()] + " " + theDate.getDate() + ", " + theDate.getFullYear();
+			
+$(".currently").html(today);
